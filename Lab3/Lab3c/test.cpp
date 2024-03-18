8078f9e5cc6d6c2de82a8a6852a15a22be574d00
Natalia <99181370+NataliaFlorek@users.noreply.github.com>
2024-03-18 23:40:26 +0100

    Delete Lab3/Lab3c/test.cpp

--- Lab3/Lab3c/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "Biblioteka.h"
-
-int main(){
-    Biblioteka *biblioteka = new Biblioteka("Biblioteka");
-    biblioteka->setData("Ostatnie zyczenie", "Andrzej Sapkowski");
-    biblioteka->printData();
-    delete biblioteka;
-    return 0;
-}
