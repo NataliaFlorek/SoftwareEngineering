a90631f1ea399480fba9658c6d322282abc55a2b
Natalia <99181370+NataliaFlorek@users.noreply.github.com>
2024-03-18 23:35:14 +0100

    Add files via upload

---
+++ Lab3/Lab3b/main.cpp
@@ -0,0 +1,11 @@
+
+#include "Student.h"
+#include "Zeszyt.h"
+#include <iostream>
+using namespace std;
+int main(){
+    Student student;
+    Zeszyt zeszyt;
+    cout << student.UzyjZeszytu(zeszyt);
+    return 0;
+}
